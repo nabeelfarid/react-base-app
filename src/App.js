00c3902da3a5012c6600800966da1b1744d20d52
Nabeel <nabeelfarid@gmail.com>
2021-02-03 20:31:04 +0500

    Update msg

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Hello from Nabeel
+        Hello from Nabeel Farid to everyone
         <p>
           
           Edit <code>src/App.js</code> and save to reload.
